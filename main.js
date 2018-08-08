956e028babfc39def6a8c8566a421b0a4ed26794
bmpitzen <bmpitzen@icloud.com>
2018-08-08 16:58:30 -0400

    Added functionality to remove value after equals

--- main.js
+++ main.js
@@ -19,47 +19,102 @@ var display = document.getElementById("display")
 
 
 seven.addEventListener('click', function(e){
+    if (display.classList.contains ("entered")){
+        display.innerText = "";
+        display.innerText += "7";
+        display.classList.remove ("entered");
+    } else {
     display.innerText += "7"
-  });
+    }});
 eight.addEventListener('click', function(e){
+    if (display.classList.contains ("entered")){
+        display.innerText = "";
+        display.innerText += "8";
+        display.classList.remove ("entered");
+    } else {
     display.innerText += "8"
-  });
+    }});
 nine.addEventListener('click', function(e){
+    if (display.classList.contains ("entered")){
+        display.innerText = "";
+        display.innerText += "9";
+        display.classList.remove ("entered");
+    } else {
     display.innerText += "9"
-  });
+    }});
 divide.addEventListener('click', function(e){
     display.innerText += "/"
   });
 four.addEventListener('click', function(e){
+        if (display.classList.contains ("entered")){
+        display.innerText = "";
+        display.innerText += "4";
+        display.classList.remove ("entered");
+    } else {
     display.innerText += "4"
-  });
+    }});
 five.addEventListener('click', function(e){
+        if (display.classList.contains ("entered")){
+        display.innerText = "";
+        display.innerText += "5";
+        display.classList.remove ("entered");
+    } else {
     display.innerText += "5"
-  });
+    }});
 six.addEventListener('click', function(e){
+        if (display.classList.contains ("entered")){
+        display.innerText = "";
+        display.innerText += "6";
+        display.classList.remove ("entered");
+    } else {
     display.innerText += "6"
-  });
+    }});
 times.addEventListener('click', function(e){
     display.innerText += "*"
   });
 one.addEventListener('click', function(e){
+    if (display.classList.contains ("entered")){
+        display.innerText = "";
+        display.innerText += "1";
+        display.classList.remove ("entered");
+    } else {
     display.innerText += "1"
-  });
+    }});
 two.addEventListener('click', function(e){
+    if (display.classList.contains ("entered")){
+        display.innerText = "";
+        display.innerText += "2";
+        display.classList.remove ("entered");
+    } else {
     display.innerText += "2"
-  });
+    }});
 three.addEventListener('click', function(e){
+    if (display.classList.contains ("entered")){
+        display.innerText = "";
+        display.innerText += "3";
+        display.classList.remove ("entered");
+    } else {
     display.innerText += "3"
-  });
+    }});
 minus.addEventListener('click', function(e){
     display.innerText += "-"
   });
 zero.addEventListener('click', function(e){
+    if (display.classList.contains ("entered")){
+        display.innerText = "";
+        display.innerText += "0";
+        display.classList.remove ("entered");
+    } else {
     display.innerText += "0"
-  });
+    }});
 decimal.addEventListener('click', function(e){
+    if (display.classList.contains ("entered")){
+        display.innerText = "";
+        display.innerText += ".";
+        display.classList.remove ("entered");
+    } else {
     display.innerText += "."
-  });
+    }});
 plus.addEventListener('click', function(e){
     display.innerText += "+"
   });
@@ -67,5 +122,10 @@ clear.addEventListener('click', function(e){
     display.innerText = ""
   });
 equals.addEventListener('click', function(e){
-    display.innerText = eval(display.innerText)
+    try {
+        display.innerText = eval(display.innerText)
+        display.classList.add ("entered")
+    } catch {
+        display.innerText = "ERROR"
+    }
   });
